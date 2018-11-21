8e2b4318315535799184c4754ee99eb54d343b25
romhartmann <romhartmann@gmail.com>
2018-11-20 19:56:41 -0800

    include build tracker

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![CircleCI](https://circleci.com/gh/RomHartmann/todo_tracker.svg?style=svg)](https://circleci.com/gh/RomHartmann/todo_tracker)
+
 Todo list tracker
 =================
 
--- crud/tests.py
+++ crud/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import datetime
+import pytz
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse_lazy
+from rest_framework.test import APIClient
+from rest_framework.status import HTTP_200_OK
+
+from crud.models import Todo
+
+TZ = pytz.timezone('US/Pacific')
+
+
+class TodoTest(TestCase):
+    """Ok, these tests could be more complete, but they really are just filler."""
+
+    def setUp(self):
+        Todo.objects.create(
+            text='A test todo',
+            state='DN',
+            due_at=datetime.datetime.now(tz=TZ)
+        )
+        self.client = APIClient()
+
+    def test_created(self):
+        first_todo = Todo.objects.get(text='A test todo')
+        self.assertEqual(first_todo.state, 'DN')
+
+    def test_list_view(self):
+        response = self.client.get(reverse_lazy('view:list_view'))
+        self.assertEqual(response.status_code, HTTP_200_OK)
